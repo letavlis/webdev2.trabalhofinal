db4638352d802dd3ff421725f795c5b9c85b0b90
Bruna Leticia <bruna.letavlis@outlook.com>
2022-09-13 17:26:29 -0300

    event seeder ok

---
+++ eventsmanager/database/seeders/EventSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('events')->insert([
+            'name' => 'Oficina de Bootstrap',
+            'eventdate' => '2022/12/10',
+        ]);
+    }
+}
