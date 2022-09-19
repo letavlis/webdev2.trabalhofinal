adbf549d1b8b4d9e063b9ec76663e3df8eb1e61b
Bruna Leticia <bruna.letavlis@outlook.com>
2022-09-19 17:46:52 -0300

    Controller EventPlanner

---
+++ eventsmanager/app/Http/Controllers/EventPlannerController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EventPlanner;
+use Illuminate\Http\Request;
+
+class EventPlannerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(){
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EventPlanner  $eventPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventPlanner $eventPlanner){
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EventPlanner  $eventPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventPlanner $eventPlanner){
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EventPlanner  $eventPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EventPlanner $eventPlanner){
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EventPlanner  $eventPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventPlanner $eventPlanner){
+        //
+    }
+}
