c5b14d804701550bbcbf4ceab6764b56ddfd1231
Bruna Leticia <bruna.letavlis@outlook.com>
2022-09-19 17:46:15 -0300

    ORM Event Planner

--- eventsmanager/app/Models/EventPlanner.php
+++ eventsmanager/app/Models/EventPlanner.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Event extends Model
+class EventPlanner extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
+    public function event() {
+        return $this->belongsTo('App\Models\Event');
+    }
     public function users() {
-        return $this->belongsToMany('\App\Models\Users', 'event_planners');
-        }
+        return $this->belongsTo('App\Models\Users');
+    }
 
 }
--- eventsmanager/app/Models/User.php
+++ eventsmanager/app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function event() {
+        return $this->belongsToMany('\App\Models\Event', 'event_planners');
+        }
+
 }
